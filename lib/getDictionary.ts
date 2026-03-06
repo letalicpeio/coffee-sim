0fd1bba91563094e7b1f4eb2758da9f35c2a4da3
letalicpeio <peioiii@hotmail.com>
2026-03-06 23:27:31 +0100

    fix type Dict for vercel build

--- lib/getDictionary.ts
+++ lib/getDictionary.ts
@@ -1,5 +1,7 @@
+export type Dict = Record<string, any>;
+
 export async function getDictionary(locale: string) {
-    const l = locale === "en" ? "en" : "es";
-    const dict = await import(`../dictionaries/${l}.json`);
-    return dict.default;
-  }
+  const l = locale === "en" ? "en" : "es";
+  const dict = await import(`../dictionaries/${l}.json`);
+  return dict.default;
+}
